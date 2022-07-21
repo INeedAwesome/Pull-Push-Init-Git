c37bc1c3410e4b963f9c6536adab79d2b2e15f07
Mattis Kjellerås <77880455+INeedAwesome@users.noreply.github.com>
2022-07-21 23:51:12 +0200

    Update PushToGithub.bat

--- PushToGithub.bat
+++ PushToGithub.bat
@@ -5,6 +5,7 @@ set /p httpsAddres=Addres to an already existing repository?:
 set /p message=Message for push?:
 @echo URL='%message%'
 
+git pull %httpsAddres%
 git add *
 git commit -m "%message%"
 git remote add origin %httpsAddres%
